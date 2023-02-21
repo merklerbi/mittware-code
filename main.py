74b5645e38a13b1f5368894d3b024ade9d75bae9
flaku <falk@draftcode.de>
2023-02-21 17:22:00 +0100

    improved by chatGPT
    - error-handling
    - logging
    - forced insecure md5 hashing (against warnings from chatGPT)

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 #!/usr/bin/env python
 
+"""_summary_
+The script begins by importing necessary modules including os, hashlib, logging, and getpass from the dotenv and os.path packages.
+
+The init_env() function uses the load_dotenv() function from dotenv to load the DBPW variable from the .env file, and raises a ValueError if the variable is not found.
+
+The compare_passwords() function takes the dbpw argument (the user-entered password) and compares its SHA-256 hash value to a pre-defined hash value stored in exception_hash.
+If the hashes match, the function logs the correct password to a file named password_check.log and prints a success message to the console.
+Otherwise, the function logs the incorrect password and prints a failure message to the console.
+
+In the main block of the script, the logging module is configured to log messages at the INFO level to a file named password_check.log.
+If the .env file exists, the init_env() function is called to retrieve the database password. If the DBPW variable is not found in the .env file, the script logs an error message and exits.
+Otherwise, the getpass() function is used to securely prompt the user for the password input. The compare_passwords() function is then called with the user-entered password as the argument.
+"""
+
 import os
-from os.path import exists
-# maybe you have to install something
 import hashlib
+import logging
+from getpass import getpass
 from dotenv import load_dotenv
 
-env_file=".env"
+ENV_FILE = os.getenv("ENV_FILE", ".env")
+
 
 def init_env():
-    load_dotenv()
-    return os.getenv('DBPW')
+    try:
+        load_dotenv(ENV_FILE)
+    except FileNotFoundError:
+        raise ValueError(f"{ENV_FILE} not found")
+    dbpw = os.getenv("DBPW")
+    if dbpw is None:
+        raise ValueError("No DBPW variable found in .env file")
+    return dbpw
 
-def compare_passwords(DBPW):
-    execption = str("7073420f60d8c5f923a92e656516a5b4")
+def compare_passwords(dbpw):
+    hashed_password = str("7073420f60d8c5f923a92e656516a5b4")
+    hashed_dbpw = hashlib.md5(dbpw.encode()).hexdigest()
 
-    if hashlib.md5(DBPW.encode()).hexdigest() == execption:
-        print(f'You got it!\nThe DBPW is {DBPW}')
+    if hashed_dbpw == hashed_password:
+        logging.info("Correct password entered")
+        print(f"You got it! The DBPW is correct: {dbpw}")
     else:
-        print(f'You failed!\nThe DBPW isn\'t {DBPW}')
+        logging.warning("Incorrect password entered")
+        print(f"You failed! The DBPW is incorrect: {dbpw}")
     exit()
 
 if __name__ == "__main__":
-    file_exists = os.path.exists(env_file)
-    if file_exists:
-        DBPW = init_env()
-    else:
-        print(f'No {env_file} provided!\n')
-        DBPW = str(input("Please give Database Password: "))
+    logging.basicConfig(filename="password_check.log", level=logging.INFO)
 
-    compare_passwords(DBPW)
+    try:
+        dbpw = init_env()
+    except ValueError as e:
+        logging.error(str(e))
+        print(f"Error: {str(e)}")
+        dbpw = getpass("Please give Database Password: ")
+
+    compare_passwords(dbpw)
